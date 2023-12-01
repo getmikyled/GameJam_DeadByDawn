febaa6b00e40ec0641a42202cded8af2657c8755
getmikyled <mikylemosquera@gmail.com>
2023-11-30 18:04:15 -0800

    Fixed Shooting Through Walls

--- Assets/Scripts/Player/Weapons/Bullet.cs
+++ Assets/Scripts/Player/Weapons/Bullet.cs
@@ -40,7 +40,7 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Obstructions"))
         {
             shooting = false;
             gameObject.SetActive(false);
